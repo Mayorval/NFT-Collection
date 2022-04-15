e06a53816ad3cc232e4f98b23cde8da8025d1273
mayorval <chukwuonyechinedu@gmail.com>
2022-04-15 20:53:50 +0200

    added all files

---
+++ smart-contracts/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x1C84dDbd2626639aeF083A84868F8deaD95C1B7a";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-beryl.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
